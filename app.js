4fa9d074d7ffa7d056280b72bebd7318cb878c03
jecamargo10 <javier_22ca@hotmail.com>
2018-11-20 10:07:17 -0500

    aa

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Cualquier CosaX2');
+  res.send('<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Metadata goes here -->
+  </head>
+  <body>
+    Holaaaaaa
+  </body>
+</html>');
 });
 
 app.get('/getMyDebtMaximo', function(req, res){
