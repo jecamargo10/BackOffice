198fa5e48a4eae7458e6c1002d6d6be285b08600
Cristian Andres Marin Durango <camd1996@gmail.com>
2019-05-08 16:13:41 -0500

    Simulator advance

--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -5,81 +5,93 @@
         <Subheader :firstMessage="'Simular mi credito'" :currentStage="null" :secondMessage="null"></Subheader>
       </div>
     </div>
-    <div class="row">
-      <div class="col-1"></div>
-      <div class="col mt-4 mb-4">
-        <div class="input-group">
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Crédito en US"
-            aria-label="Recipient's username with two button addons"
-            aria-describedby="button-addon4"
-            min="0"
-            max="25000"
-          >
-          <div class="input-group-append" id="button-addon4">
-            <button class="btn btn-outline-primary" type="button">+</button>
-            <button class="btn btn-outline-primary" type="button">-</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-1"></div>
-    </div>
+
     <div class="row">
       <div class="col pb-3 pt-3" style="background-color:#eee">
         <div class="row">
           <div class="col">
-            <h6>Ajustar número de cuotas</h6>
+            <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-credit">Credito:</span>
+              </div>
+              <input
+                :value="amount.current"
+                type="number"
+                readonly
+                :min="amount.min"
+                :max="amount.max"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-credit"
+              >
+            </div>
           </div>
         </div>
         <div class="row">
-          <div class="col-1"></div>
           <div class="col">
-            <div class="row">
-              <div class="col">
-                <input type="range" class="custom-range" min="0" max="5" step="1" id="customRange3">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" v-text="'$ '+amount.min"></span>
               </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <label for="customRange3">12</label>
-              </div>
-              <div class="col">
-                <label for="customRange3">24</label>
-              </div>
-              <div class="col">
-                <label for="customRange3">36</label>
-              </div>
-              <div class="col">
-                <label for="customRange3">48</label>
-              </div>
-              <div class="col">
-                <label for="customRange3">60</label>
-              </div>
-              <div class="col">
-                <label for="customRange3">72</label>
+              <input
+                type="range"
+                readonly
+                v-model="amount.current"
+                class="custom-range"
+                :min="amount.min"
+                :max="amount.max"
+                step="1"
+              >
+              <div class="input-group-append">
+                <span class="input-group-text" v-text="'$ '+amount.max"></span>
               </div>
             </div>
           </div>
-          <div class="col-1"></div>
         </div>
       </div>
     </div>
-    <div class="row mt-4 pb-4 border-bottom">
-      <div class="col-1"></div>
-      <div class="col">
+
+    <div class="row">
+      <div class="col pb-3 pt-3" style="background-color:#eee">
         <div class="row">
           <div class="col">
-            <h5>Valor cuota mensual</h5>
+            <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-credit"># Cuotas:</span>
+              </div>
+              <input
+                v-model="monthlyPaymentQuantity.current"
+                type="number"
+                :min="monthlyPaymentQuantity.min"
+                :max="monthlyPaymentQuantity.max"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-credit"
+              >
+            </div>
           </div>
         </div>
-        <div class="row mt-2">
+        <div class="row">
           <div class="col">
-            <h3>US $500</h3>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" v-text="monthlyPaymentQuantity.min"></span>
+              </div>
+              <input
+                type="range"
+                v-model="monthlyPaymentQuantity.current"
+                class="custom-range"
+                :min="monthlyPaymentQuantity.min"
+                :max="monthlyPaymentQuantity.max"
+                step="12"
+              >
+              <div class="input-group-append">
+                <span class="input-group-text" v-text="monthlyPaymentQuantity.max"></span>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="row mt-2">
+        <div class="row">
           <div class="col">
             <div class="custom-control custom-switch">
               <input type="checkbox" class="custom-control-input" id="customSwitch1">
@@ -88,19 +100,64 @@
           </div>
         </div>
       </div>
-      <div class="col-1"></div>
     </div>
+    
+    <div class="row">
+      <div class="col pb-3 pt-3" style="background-color:#eee">
+        <div class="row">
+          <div class="col">
+            <div class="input-group input-group-sm mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-credit">Cuota mensual:</span>
+              </div>
+              <input
+                v-model="monthlyPayment.current"
+                type="number"
+                :min="monthlyPayment.min"
+                :max="monthlyPayment.max"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-credit"
+              >
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" v-text="'$ '+monthlyPayment.min"></span>
+              </div>
+              <input
+                type="range"
+                v-model="monthlyPayment.current"
+                class="custom-range"
+                :min="monthlyPayment.min"
+                :max="monthlyPayment.max"
+                step="0.05"
+              >
+              <div class="input-group-append">
+                <span class="input-group-text" v-text="'$ '+monthlyPayment.max"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="row">
       <div class="col-1"></div>
       <div class="col">
         <div class="row mt-3">
           <div class="col">
-             <button type="submit" class="btn btn-outline-primary btn-lg">Aplicar</button>
+            <button type="submit" class="btn btn-outline-primary btn-lg">Aplicar</button>
           </div>
         </div>
         <div class="row mt-4">
           <div class="col">
-            <h5 class="text-center text-primary"><u>Cancelar</u></h5>
+            <h5 class="text-center text-primary">
+              <u>Cancelar</u>
+            </h5>
           </div>
         </div>
       </div>
@@ -111,25 +168,64 @@
 
 
 <script>
-import Subheader from '../components/Subheader.vue'
+import Subheader from "../components/Subheader.vue";
 export default {
   name: "SettingPage", //nombre con el cual se usa el componente
-  props: ["asasda"], //propiedades requeridas(o no) para el uso del componente
+  props: {}, //propiedades requeridas(o no) para el uso del componente
   components: {
     Subheader
   },
   data() {
     //data reactiva del componente
-    return {};
+    return {
+      amount: {
+        current: undefined,
+        max: undefined,
+        min: undefined
+      },
+      monthlyPayment: {
+        current: 500,
+        max: 500,
+        min: 280
+      },
+      monthlyPaymentQuantity: {
+        current: 60,
+        max: 60,
+        min: 12
+      },
+      tasa: 0.1665,
+    };
   },
   created() {
-    //lo que pasa cuando se crea el componente  aca se ponen la consulta a servicios
+    this.amount.min = this.calculateAmount(this.monthlyPayment.min,this.monthlyPaymentQuantity.min);
+    this.amount.max = this.calculateAmount(this.monthlyPayment.max,this.monthlyPaymentQuantity.max);
+    this.amount.current = this.amount.max;
   },
   mounted() {
     //lo que pasa cuando se monta el HTML al browser
   },
   methods: {
-    //metodos
+    calculateAmount(c,p) {
+      let a = this.tasa/12
+      let b = Math.pow((1 + (a)), p)
+      return Math.ceil(c * ((b - 1) / (a * b)))
+    },
+    calculateValues(c,p) {
+      let a = this.tasa/12
+      let b = Math.pow((1 + (a)), p)
+      return Math.ceil(c * ((b - 1) / (a * b)))
+    },
+    changeAmount() {
+      this.amount.current = this.calculateAmount(this.monthlyPayment.current,this.monthlyPaymentQuantity.current)
+    }
+  },
+  watch: {
+    'monthlyPayment.current': function() {
+      this.changeAmount();
+    },
+    'monthlyPaymentQuantity.current': function() {
+      this.changeAmount();
+    }
   }
 };
 </script>
