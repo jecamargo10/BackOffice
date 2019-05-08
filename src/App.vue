aa3d5c46cbadf76afec148ba3eaf12a1aa0ddfc3
Cristian Andres Marin Durango <camd1996@gmail.com>
2019-05-07 19:54:20 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <header-auto-facil></header-auto-facil>
+    <router-link to="/s">Go to Foo</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HeaderAutoFacil from './components/HeaderAutoFacil.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HeaderAutoFacil
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        Login page
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'login-page', //nombre con el cual se usa el componente
+    props: ['asasda'], //propiedades requeridas(o no) para el uso del componente
+    data () {//data reactiva del componente
+        return {
+
+        }
+    },
+    created(){//lo que pasa cuando se crea el componente  aca se ponen la consulta a servicios
+
+    },
+    mounted (){//lo que pasa cuando se monta el HTML al browser
+
+    },
+    methods: {//metodos
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
