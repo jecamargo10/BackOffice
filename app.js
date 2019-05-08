a02ddb61bac4ebaa97a48f5e9e4628466f56b704
jecamargo10 <javierecam22@gmail.com>
2019-05-07 20:34:10 -0500

    the end
    asdasd

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 app = express();
 
-app.use(express.static('dist'))
+//app.use(express.static('dist'))
 
 var port = 80;
 
 
-
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/dist/index.html');
+app.use(express.static(__dirname + '/dist'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 
