4a7df7783618d99a3de23f00f1d39a0957f21c7b
Breakermoob <breakermoob@hotmail.com>
2019-05-08 01:06:33 -0500

    Setting

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import LoginPage from './pages/Login.vue'
 import LobbyPage from './pages/Lobby.vue'
 import RegistryPage from './pages/Registry.vue'
 import ResultPage from './pages/Result.vue'
+import SettingPage from './pages/Setting.vue'
 
 // 2. Define some routes
 // Each route should map to a component. The "component" can
@@ -19,7 +20,8 @@ const routes = [
   { path: '/', component: LoginPage},
   { path: '/lobby', component: LobbyPage},
   { path: '/registry', component: RegistryPage },
-  { path: '/result', component: ResultPage }
+  { path: '/result', component: ResultPage },
+  { path: '/setting', component: SettingPage }
 ]
 
 // 3. Create the router instance and pass the `routes` option
--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -1,9 +1,77 @@
 <template>
    <section>
       <div class="row">
+         <div class="col" style="background-color:#aaa">
+
+         </div>
+      </div>
+      <div class="row">
          <div class="col">
-            
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
+         </div>
+      </div>
+      <div class="row">
+         <div class="col">
          </div>
       </div>
    </section>
 </template>
+
+
+<script>
+export default {
+  name: "SettingPage", //nombre con el cual se usa el componente
+  props: ["asasda"], //propiedades requeridas(o no) para el uso del componente
+  data() {
+    //data reactiva del componente
+    return {};
+  },
+  created() {
+    //lo que pasa cuando se crea el componente  aca se ponen la consulta a servicios
+  },
+  mounted() {
+    //lo que pasa cuando se monta el HTML al browser
+  },
+  methods: {
+    //metodos
+  }
+};
+</script>
+
+<style scoped>
+</style>
