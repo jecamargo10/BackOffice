628b02701347f7bf982c216ea8dbd2e7b95d4224
jecamargo10 <javierecam22@gmail.com>
2019-05-08 13:30:01 -0500

    asd
    asd

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ app = express();
 
 //app.use(express.static('dist'))
 
-app.configure(function(){
   //server.use('/media', express.static(__dirname + '/media'));
   app.use(express.static(__dirname + '/dist'));
 //  app.use(express.static('dist'))
-});
 
 
 var port = 80;
