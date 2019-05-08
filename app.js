a0fa5b5b24c14081c32efe1390ecba016799f958
jecamargo10 <javierecam22@gmail.com>
2019-05-07 20:43:04 -0500

    TEST
    TEST

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 app = express();
 
-//app.use(express.static('dist'))
+app.use(express.static('dist'))
 
 var port = 80;
 
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+
+app.get('/', function(req, res){
+  res.sendfile( './dist/index.html');
 });
 
 
