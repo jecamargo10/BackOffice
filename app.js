cddab7493becc9263afdd03959f9f92d13ac4566
jecamargo10 <javierecam22@gmail.com>
2019-05-07 20:26:02 -0500

    making available the html
    asd

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ app.use(express.static('dist'))
 var port = 80;
 
 
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/dist/index.html');
+});
+
+
+
 app.listen(port, function(){
     console.log('server started '+ port);
 });
