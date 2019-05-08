9ca94f399b85ddd320a12aa898169f942658320e
jecamargo10 <javierecam22@gmail.com>
2019-05-08 01:46:29 -0500

    o
    u

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ app = express();
 
 app.use(express.static('dist'))
 
-var port = 80;
+var port = 8080;
 
 var path = require("path");
+console.log(__dirname);
+if (!__dirname)
+{
+  __dirname = path.resolve(path.dirname(''));
 
+}
 
 app.get('/', function(req, res){
 //  const index = path.join(__dirname, 'dist', 'index.html');
 //  res.sendFile(index);
-res.sendFile(__dirname + '/dist/index.html'); 
+res.sendFile(__dirname + '/dist/index.html');
 
 });
 
